db31073e9762778ae54aa03cda8089b5816f1ab6
EC2 Default User <ec2-user@ip-172-31-8-208.ec2.internal>
2022-04-27 12:17:53 +0000

    SecoundCommit

--- MemoMain.rb
+++ MemoMain.rb
@@ -5,27 +5,27 @@ def firstFunction
     _MakeOrOpen = gets.to_i
     if _MakeOrOpen == 1
         p "新規ファイル作成します。ファイル名を入力してください"
-        filename = gets
+        filename = gets.chomp
         csvfilename = filename+".csv"
         CSV.open(csvfilename,"w")do |csv|
             puts "メモ内容を入力してください"
-            loop{
-                memo_contents = gets.chomp
+            while input_contents = gets
+                memo_contents = input_contents.chomp
                 csv << [memo_contents]
-            }
+            end
         end
         
     elsif _MakeOrOpen == 2
         p "既存ファイルを開きます。ファイル名を入力してください"
-        filename = gets
+        filename = gets.chomp
         csvfilename = filename+".csv"
         puts csvfilename
         CSV.open(csvfilename,"a")do |csv|
             puts "メモ内容を入力してください"
-            loop{
-                memo_contents = gets.chomp
+            while input_contents = gets
+                memo_contents = input_contents.chomp
                 csv << [memo_contents]
-            }
+            end
         end
 
     else
